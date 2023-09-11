09d94fb637c01d1b55943d08aadc240d657fec97
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2023-09-11 11:10:18 -0500

    Add files via upload

---
+++ scroll_infinito/app/pages/home/home.page.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+//import { error } from 'console';
+import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
+import { ShareModule } from 'src/app/share/share.module';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone:true,
+  imports:[IonicModule, CommonModule, FormsModule, ShareModule]
+})
+export class HomePage implements OnInit {
+characters:any[]=[];
+params = {} as any;
+  constructor(
+  private rickAndMortySvc: RickAndMortyService
+  ) { }
+
+  ngOnInit() {
+  this.params.page=0;
+  this.getCharacters()  
+  }
+  
+  //funcion de cargar todos los datos
+  getCharacters(event?:any) {
+    this.params.page +=1;
+    this.rickAndMortySvc.getCharacters(this.params).subscribe(
+    {
+    next:(res:any)=>{
+    this.characters.push(...res.results)
+    if(event) event.target.complete();
+    },
+    error: (error: any)=>{
+      if(event) event.target.complete();
+    }
+    }
+    )
+  
+  }
+  
+
+  
+  }
+
+}
---
+++ scroll_infinito/app/services/rick-and-morty.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RickAndMortyService {
+
+  constructor(private http: HttpClient) { }
+  getCharacters(params:any){
+  return this.http.get(environment.baseUrl + environment.characters, {params})
+  }
+}
